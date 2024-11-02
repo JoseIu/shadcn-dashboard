ebdbb3e1bb4325b2672d4b1605355861e576d93c
Joselu <joseluissgz97@gmail.com>
2024-11-02 22:51:06 +0100

    add: ButtonPage

---
+++ src/app/dashboard/button/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button';
+
+const ButtonPage = () => {
+  return (
+    <div>
+      ButtonPage
+      <Button variant="outline">Button</Button>
+    </div>
+  );
+};
+
+export default ButtonPage;
